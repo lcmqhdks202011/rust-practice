25929adbc17a112b37a433277caf8689ce56e645
lcmqhdks202011 <lcmqhdks202011@gmail.com>
2025-12-07 13:00:43 +0900

    crossbeam

--- src/read.rs
+++ src/read.rs
@@ -1,10 +1,12 @@
 use crate::CHUNK_SIZE;
+
 use std::fs::File;
 use std::io::{self, BufReader, Read, Result};
 // use std::sync::{Arc, Mutex};
-use std::sync::mpsc::Sender;
+// use std::sync::mpsc::Sender;
+use crossbeam::channel::Sender;
 
-pub fn read_loop(infile: &str, stats_tx: Sender<Vec<u8>>) -> Result<()> {
+pub fn read_loop(infile: &str, stats_tx: Sender<usize>, write_tx: Sender<Vec<u8>>) -> Result<()> {
     let mut reader: Box<dyn Read> = if !infile.is_empty() {
         Box::new(BufReader::new(File::open(infile)?))
     } else {
@@ -20,13 +22,21 @@ pub fn read_loop(infile: &str, stats_tx: Sender<Vec<u8>>) -> Result<()> {
             Err(_) => break,
         };
 
-        // todo: send this buffer to the stats thread
-        if stats_tx.send(Vec::from(&buffer[..num_read])).is_err() {
+        let _ = stats_tx.send(num_read);
+
+        if write_tx.send(Vec::from(&buffer[..num_read])).is_err() {
             break;
         }
+
+        // // todo: send this buffer to the stats thread
+        // if stats_tx.send(Vec::from(&buffer[..num_read])).is_err() {
+        //     break;
+        // }
     }
 
-    let _ = stats_tx.send(Vec::new());
+    let _ = stats_tx.send(0);
+    let _ = write_tx.send(Vec::new());
+    // let _ = stats_tx.send(Vec::new());
     // // todo: send an empty buffer to the stats threa
     // let mut quit = quit.lock().unwrap();
     //
--- src/stats.rs
+++ src/stats.rs
@@ -1,27 +1,25 @@
 use std::io::Result;
 // use std::sync::{Arc, Mutex};
-use std::sync::mpsc::{Receiver, Sender};
+// use std::sync::mpsc::{Receiver, Sender};
+use crossbeam::channel::Receiver;
 
-pub fn stats_loop(
-    silent: bool,
-    stats_rx: Receiver<Vec<u8>>,
-    write_tx: Sender<Vec<u8>>,
-) -> Result<()> {
+pub fn stats_loop(silent: bool, stats_rx: Receiver<usize>) -> Result<()> {
     let mut total_bytes = 0;
     loop {
         // // todo receive the vector of bytes
         // let buffer: Vec<u8> = Vec::new();
-        let buffer = stats_rx.recv().unwrap();
-        let num_bytes = buffer.len();
+        // let buffer = stats_rx.recv().unwrap();
+        // let num_bytes = buffer.len();
+        let num_bytes = stats_rx.recv().unwrap();
         total_bytes += num_bytes;
 
         if !silent {
             eprint!("\r{}", total_bytes);
         }
 
-        if write_tx.send(buffer).is_err() {
-            break;
-        }
+        // if write_tx.send(buffer).is_err() {
+        //     break;
+        // }
 
         if num_bytes == 0 {
             break;
